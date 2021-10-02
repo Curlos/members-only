24d7331fe08a001d0cbc32e6a9304141ab91b2b1
Carlos Martinez <curlosmart@gmail.com>
2021-10-01 22:36:34 -0400

    setup passport strategies to sign up/login

--- server/routers/membersRouter.js
+++ server/routers/membersRouter.js
@@ -1,10 +1,102 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const { User, Message } = require('../models/models')
+const bcrypt = require("bcryptjs")
 const router = express.Router()
+
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      console.log('finding nigga')
+      if (err) { 
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      } else {
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            console.log('ello mate')
+            // passwords match! log user in
+            return done(null, user)
+          } else {
+            // passwords do not match!
+            return done(null, false, { message: "Incorrect password" })
+          }
+        })
+        return done(null, user);
+      }
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 router.get('/', async(req, res) => {
   res.json({user: 'user'})
 })
+
+router.get("/log-out", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+router.post("/sign-up", async (req, res, next) => {
+  const newUsername = req.body.username
+  const newPassword = req.body.password
+
+  const userFound = await User.find({username: newUsername})
+
+  if (userFound.length > 0) {
+    console.log('fuck')
+    return res.json({error: 'Username taken'})
+  }
+
+  bcrypt.hash(newPassword, 10, (err, hashedPassword) => {
+    if (err) {
+      return err
+    }
+
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+      membershipStatus: 'Anonymous',
+      messages: []
+    }).save(err => {
+      if (err) { 
+        return next(err);
+      }
+      res.redirect("/members");
+    });
+  })
+});
+
+router.get('/success', (req, res) => {
+  res.json({user: req.user})
+})
+
+router.get('/failure', (req, res) => {
+  console.log(req.user)
+  res.json({user: req.user})
+})
+
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/members//success",
+    failureRedirect: "/members/failure"
+  })
+);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const app = express()
+const session = require("express-session");
+const passport = require("passport");
 const cors = require('cors')
-
 const PORT = process.env.PORT || 8888
 const membersRouter = require('./routers/membersRouter')
 const database = require('./database/connection')
 
+const app = express()
+
 
 app.use(cors())
 app.use(express.json())
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/members', membersRouter)
 
 app.listen(PORT, () => {
