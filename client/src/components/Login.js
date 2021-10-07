4a43bde4ca4ddfe1a8bcfca3db1ad2e25ea5d1ae
Carlos Martinez <curlosmart@gmail.com>
2021-10-06 21:22:53 -0400

    Sign Up and Log In component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,60 @@
 import React, { useState } from 'react';
+import LoginContext from '../contexts/LoginContext';
+import UserContext from '../contexts/UserContext';
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
 
 const Login = () => {
+  const { isLoggedIn, setIsLoggedIn } = React.useContext(LoginContext)
+  const { loggedInUser, setLoggedInUser } = React.useContext(UserContext)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  console.log(isLoggedIn)
+  console.log(loggedInUser)
+
+  const handleUsernameChange = (e) => {
+    console.log(e.target.value)
+    setUsername(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    console.log(e.target.value)
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(username)
+    console.log(password)
+
+    const body = {
+      "username": username,
+      "password": password
+    }
+
+    const response = await axios.post('http://localhost:8888/users/login', body)
+
+    console.log(response)
+
+    if (response.data.result === 'success') {
+      setIsLoggedIn(true)
+
+      
+    } else {
+      
+    }
+
+    return response.data
+  }
 
   return (
-    <div>
-      Login
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>Log in to your account</div>
+      <input type="text" value={username} placeholder="Username" onChange={handleUsernameChange}></input>
+      <input type="password" value={password} placeholder="Password" onChange={handlePasswordChange}></input>
+      <button onClick={handleSubmit}>Log In</button>
+    </form>
   )
 }
 
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,59 @@
 import React, { useState } from 'react';
+import axios from 'axios'
+import LoginContext from '../contexts/LoginContext';
 
 const SignUp = () => {
 
+  const { isLoggedIn, setIsLoggedIn } = React.useContext(LoginContext)
+
+  const [fullName, setFullName] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  console.log(isLoggedIn)
+
+  const handleFullNameChange = (e) => {
+    console.log(e.target.value)
+    setFullName(e.target.value)
+  }
+
+  const handleUsernameChange = (e) => {
+    console.log(e.target.value)
+    setUsername(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    console.log(e.target.value)
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    
+    console.log(fullName)
+    console.log(username)
+    console.log(password)
+
+    const body = {
+      fullName,
+      username, 
+      password
+    }
+
+    const data = await axios.post('http://localhost:8888/users/sign-up', body)
+
+    console.log(data)
+    return data
+  }
+
   return (
-    <div>
-      Sign Up
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>Create your account</div>
+      <input type="text" value={fullName} placeholder="Full Name" onChange={handleFullNameChange}></input>
+      <input type="text" value={username} placeholder="Username" onChange={handleUsernameChange}></input>
+      <input type="password" value={password} placeholder="Password" onChange={handlePasswordChange}></input>
+      <button onClick={handleSubmit}>Sign Up</button>
+    </form>
   )
 }
 
